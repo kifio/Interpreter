5c0072f91dbd31730a17c30f89f13a2047fcb1bf
kifio <kg.of.morphine@gmail.com>
2020-11-19 19:08:46 +0300

    Set previous code placeholder

--- gui/src/main/java/view/Editor.java
+++ gui/src/main/java/view/Editor.java
@@ -99,13 +99,7 @@ public class Editor extends JFrame {
 
     private void addInput() {
         JEditorPane input = new JTextPane(new InputDocument(highlighter, listener));
-        StringBuilder sb = new StringBuilder();
-        sb.append("var n = 1\n");
-        for (int i = 0; i < 15000; i++) {
-            sb.append("var n = n + 1\n");
-        }
-        sb.append("out n");
-        input.setText(sb.toString());
+        input.setText(CODE_PLACEHOLDER);
         addEditorPane(input,
                 getContentPane(),
                 BorderFactory.createMatteBorder(0, 0, 1, 0, Colors.BACKGROUND),
